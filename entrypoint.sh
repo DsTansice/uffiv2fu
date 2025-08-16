30ca186b80f7611b34d43df27ea462c80a1d8444
零式的天空 <246009756@qq.com>
2025-08-16 21:39:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
-TROJAN_WSPATH='/trojan'
-SS_WSPATH='/shadowsocks'
+UUID='e4286731-3e98-45d5-88d4-e2ed7d0b2da7'
+VMESS_WSPATH='/vme'
+VLESS_WSPATH='/vle'
+TROJAN_WSPATH='/tro'
+SS_WSPATH='/sha'
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
